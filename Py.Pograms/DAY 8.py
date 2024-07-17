8b4126ee7f62e1fbba0f6dcc663154aaf80be805
shailyy <166197783+ShailyBhargava@users.noreply.github.com>
2024-07-17 11:04:39 -0700

    Add files via upload

---
+++ Py.Pograms/DAY 8.py
@@ -0,0 +1,15 @@
+def min_diff(a):
+    a = sorted(a)
+    size = len(a)
+    min_diff = 9999*999
+
+    for i in range(size-1):
+        if(a[i+1] - a[i] < min_diff):
+             min_diff = a[i+1] - a[i]
+
+    return min_diff
+
+
+a= [8,4,31,12,45,67,48,69,5]
+
+print("Mininmum difference Between elements of an array is " ,min_diff(a))
