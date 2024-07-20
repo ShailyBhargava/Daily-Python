877965385f37c5f8b2ed6f5053a95ec2e3cfa415
shailyy <166197783+ShailyBhargava@users.noreply.github.com>
2024-07-20 23:35:15 +0530

    Add files via upload

---
+++ Py.Pograms/DAY 11 .py
@@ -0,0 +1,10 @@
+# takeout length of last word of the string
+
+def last_word_len(A):
+    a = A.split()
+    #print(a)
+    last_word_len = a[-1]
+    print( last_word_len)
+
+A ='handsome is called jin'
+last_word_len(A)
